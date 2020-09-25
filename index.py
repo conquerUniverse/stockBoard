8ff241f64d43033ac74017e73840f672ae12da01
pankaj pundir <pankaj.pundir369@gmail.com>
2020-09-26 03:51:07 +0530

    from app import server
    Added

--- index.py
+++ index.py
@@ -8,6 +8,8 @@ from dash.dependencies import Input, Output, State
 
 from app import app
 from AppData import updateData,dashBoard,analysis
+from app import server
+
 
 # stockBorad lib imports
 from scripts.StockBoard import StockBoard
