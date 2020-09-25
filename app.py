a469cc1b6ca72bada489f319621c2ea200439884
Pankaj Pundir <alvin@localhost.localdomain>
2020-09-26 00:07:33 +0530

    heroku fix 1

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import dash_html_components as html
 import dash_core_components as dcc
 import dash_table
 from dash.dependencies import Input, Output
-from app import server
 
 
 # stockBorad lib imports
@@ -151,5 +150,5 @@ app.layout = html.Div(
     children = [navbar,content,footer]
     )
 
-if __name__ == '__main__':
-    app.run_server()
+# if __name__ == '__main__':
+server = app.server
