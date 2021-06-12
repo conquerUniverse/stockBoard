80e397f1f5d94619c38bb906c4cd4564580c2a42
pankaj-pundir <pankaj.pundir369@gmail.com>
2021-06-13 01:50:17 +0530

    multiprocess

--- AppData/tradingStrategies.py
+++ AppData/tradingStrategies.py
@@ -12,7 +12,9 @@ from dash.dependencies import Input, Output, State
 # ctx = dash.callback_context
 from dash_table import DataTable
 import pandas as pd
-pageHeader = dbc.CardHeader("Trading Strategies")
+from dash.exceptions import PreventUpdate
+
+from tradingStrategies.backtestFast import BackTest
 
 path = "tradingStrategies/scripts/"
 path_results = "tradingStrategies/results/"
@@ -55,19 +57,22 @@ popUpResult =  dbc.Modal(
     [Output("modalHeader", "children"),
     Output("markdownCode", "children"),
     Output("modal", "is_open")],
-    [Input("close", "n_clicks")]+
-    [Input(i+"_code", "n_clicks") for i in scripts_name],
+    [Input("close", "n_clicks"),
+    Input("show_code", "n_clicks")],
+    [
+    State("strategy", "value"),
     State("modal", "is_open")
+    ]
 )
-def toggle_modal(is_open,*args):
+def toggle_modal(is_open,show_code,strategy,*args):
     changed = ctx.triggered[0]['prop_id'].split('.')[0]
-    if 'close' in changed or changed == '':
+    if 'close' in changed or changed == '' or strategy is None:
         return '','',False
 
     # code display button
-    name = changed.replace('_code','')
-    code = "```python\n"+open(os.path.join(path,name+'.py')).read()+"\n```"
-    return name,code,True
+    # name = changed.replace('_code','')
+    code = "```python\n"+open(os.path.join(path,strategy+'.py')).read()+"\n```"
+    return strategy,code,True
 
 
    
@@ -75,43 +80,78 @@ def toggle_modal(is_open,*args):
     [Output("modalHeaderResult", "children"),
     Output("modalBodyResult", "children"),
     Output("modalResult", "is_open")],
-    [Input("closeResult", "n_clicks")]+
-    [Input(i, "n_clicks") for i in scripts_name],
+    [Input("closeResult", "n_clicks"),
+    Input("backtestData", "n_clicks")
+    ],
+    [State("strategy", "value"),
     State("modalResult", "is_open")
+    ]
 )
-def toggle_modal_result(is_open,*args):
+def toggle_modal_result(is_open,backtestData,strategy,*args):
     # print("button clicked")
     changed = ctx.triggered[0]['prop_id'].split('.')[0]
-    if 'close' in changed or changed == '':
+    if 'close' in changed or changed == '' or strategy is None:
         return '','',False
 
+    try:
+        df = pd.read_csv(os.path.join(path_results,strategy+'.csv'))
+        df.sort_values('profitPercent',ascending=False,inplace=True)
+        df.dropna()
+        df =df[['name','profitPercent','totalInvestment', 'totalNetWorth', 'NumberOfTrades', 'totalBuy',
+        'totalSell', 'currentStocksInHandValue']]
 
-    df = pd.read_csv(os.path.join(path_results,changed+'.csv'))
-    df.sort_values('profitPercent',ascending=False,inplace=True)
-    df.dropna()
-    df =df[['name','profitPercent','totalInvestment', 'totalNetWorth', 'NumberOfTrades', 'totalBuy',
-       'totalSell', 'currentStocksInHandValue']]
+        cols = [{"name":i,"id":i,} for i in df.columns]
+        data = df.to_dict('records')
 
-    cols = [{"name":i,"id":i,} for i in df.columns]
-    data = df.to_dict('records')
-
-    performance = dbc.Container(
-        DataTable(
-        columns = cols, data = data,page_size = 30,
-    sort_action = 'native',filter_action='native',
-    style_header={'backgroundColor':'black'},
-    style_cell ={'backgroundColor':'black'}, )
-    )
-    return changed+" - "+str(len(df)),performance,True
+        performance = dbc.Container(
+            DataTable(
+            columns = cols, data = data,page_size = 30,
+        sort_action = 'native',filter_action='native',
+        style_header={'backgroundColor':'black'},
+        style_cell ={'backgroundColor':'black'}, )
+        )
+        return strategy+" - "+str(len(df)),performance,True
+    except:
+        return strategy,dbc.Label("Backtest data not available",className="h6"),True
 
 # dynamically generate the strategies code
 pageBody = dbc.CardBody([dbc.Row([
-    dbc.Col(dbc.Label(i),className=" col-md-6 col-lg-8 col-sm-6 col-xs-12 p-0 m-0"),
-    dbc.Col(dbc.Button("Details",id=i,className="btn btn-block "),className="h6 col-md-3 col-lg-2 col-sm-3 col-xs-12 p-0 m-0  m-xs-0"),
-    dbc.Col(dbc.Button("Show Code", id=i+"_code",className="btn btn-block"),className="h6 col-md-3 col-lg-2 col-sm-3 col-xs-12 p-0 m-0  m-xs-0")
-]
+    dbc.Col(dbc.Label(i),className=" col-md-6 col-lg-8 col-sm-6 col-xs-12 p-0 m-0")]
 ,no_gutters=True,justify="around",className="border mb-1 row-fluid "+np.random.choice(border_color)) for i in scripts_name])
 
+dropDownStrategies = dcc.Dropdown(id='strategy', options=[
+    {'label':i,'value':i} for i in scripts_name],
+            value = None, style={ 'color': 'black',
+                                      'background-color': 'white',
+                                      'width':'20em'
+                                    },
+                                    # className="col"
+                                     )
+
+pageHeader = dbc.CardHeader([dbc.Label("Trading Strategies",className="mr-2 mr-auto"),
+    dropDownStrategies,
+    dbc.Col(dbc.Button("Details",id='backtestData',className="btn btn-block "),className="h6 col-md-1 col-lg-1 col-sm-3 col-xs-6 p-0 m-0  m-xs-0"),
+    dbc.Col(dbc.Button("Show Code", id="show_code",className="btn btn-block"),className="h6 col-md-1 col-lg-1 col-sm-3 col-xs-6 p-0 m-0  m-xs-0"),
+    dbc.Col(dbc.Button("Run Backtest", id="runBackTest",className="btn btn-block"),className="h6 col-md-2 col-lg-2 col-sm-3 col-xs-6 p-0 m-0  m-xs-0")],
+    className="row"
+    )
+
+alertView = dbc.Alert(id="alert",className="row",color='success',is_open=False, dismissable=True)
+
+@app.callback([Output('alert','is_open'),Output('alert','children')]
+,Input('runBackTest','n_clicks'),
+State("strategy", "value"))
+def runBackTest(click,strategy):
+    if click is None:
+        raise PreventUpdate
+    bt = BackTest("data/stockData/daily/")
+    bt.runMultiprocessing(strategy,True)
+    return True,'backtest Completed'
+    
+
 layout = dbc.Container([
-    dbc.Card([pageHeader,pageBody],outline=True,className="w-100 p-0 m-0"),popUp,popUpResult]
+    dbc.Card([pageHeader,alertView,pageBody],outline=True,className="w-100 p-0 m-0"),
+    
+    popUp,popUpResult,
+    ]
     ,fluid=True,className="w-100 p-0 m-0") 
---
+++ tradingStrategies/backtestFast.py
@@ -0,0 +1,147 @@
+import os
+import pandas as pd
+from multiprocessing import Process,Queue,Lock
+import importlib
+
+class BackTest:
+    def __init__(self,loc,partition = 4):
+        self.loc = loc
+        self.allStockList = os.listdir(self.loc)
+        self.importSource = 'tradingStrategies.scripts.'
+        self.stockVal = 'close'
+        self.scriptPath = "scripts"
+        self.savePath = "tradingStrategies/results"
+        self.partition = partition
+        self.numberOfStocks = len(self.allStockList)
+    
+    def getPartition(self,):
+        total = self.numberOfStocks 
+        start = 0
+        li = []
+        for i in range(self.partition):
+            end = (i+1)*total//self.partition
+            li.append((start,end))
+            start = end
+        return li
+    
+    def parseAction(self,a):
+        a = a.lower()
+        if a in ['buy','sell']:
+            return a
+        return ''
+    
+    def getTradeSummary(self,df):
+        totalBuy = 0
+        totalSell = 0
+        totalInvestment = 0
+        totalNetWorth = 0
+        NumberOfTrades = 0
+        stocksInHand = 0 # shorting is not allowed
+        
+        # assert len(df) == len(trades),"len of trade should be equal to stock result"
+        L = len(df)
+
+        
+        for i in range(L):
+            # trade = trades[i]
+            stock = df.iloc[i]
+            trade = df[['actions','quantity']].iloc[i].to_dict()
+            
+            action = self.parseAction(trade['actions'])
+            qty  = int(0 if trade['quantity'] == '' else trade['quantity'])
+            
+            if action == 'buy':
+                totalBuy += qty
+                totalInvestment += qty*stock[self.stockVal]
+                stocksInHand += qty
+                
+            if action == 'sell':
+                
+                totalSell += qty  
+                totalNetWorth += qty*stock[self.stockVal]
+                assert stocksInHand >= qty,"trying to sell more than you have"
+                stocksInHand -= qty
+                
+            if action in ['buy','sell']:
+                NumberOfTrades += 1
+        
+        ans = {}
+        ans["totalInvestment"] = totalInvestment
+        ans["totalNetWorth"] = totalNetWorth
+        ans["NumberOfTrades"] = NumberOfTrades
+        ans["totalBuy"] = totalBuy
+        ans["totalSell"] = totalSell
+        ans["currentStocksInHandValue"] = 0
+        ans['stocksInHand']  = stocksInHand
+        return ans
+        
+    def getScriptsName(self):
+        names = [i[:-3] for i in os.listdir(self.scriptPath) if i[-3:] == '.py' and i != '__init__.py']
+        return names
+    
+    def backtestMultiprocessing(self,func,stocksList,Q=None):
+        scriptFunction = func
+        fin_ans = []
+        for i in stocksList:
+            df = pd.read_csv(os.path.join(self.loc,i))
+            trades = pd.DataFrame(scriptFunction(df))
+            res = {}
+            try:
+                trades = pd.merge(df,trades,how='inner',on='timestamp')
+                
+            except:
+                trades = pd.DataFrame()
+            res = self.getTradeSummary(trades)
+            res["name"] = i.split('.')[0]
+            res["stockGrowth"] = (df.iloc[-1][self.stockVal] - df.iloc[0][self.stockVal])/df.iloc[0][self.stockVal]
+
+            if res['stocksInHand'] > 0:
+                res["currentStocksInHandValue"] = df.iloc[-1][self.stockVal]*res['stocksInHand']
+            try:
+                res["profitPercent"] = (res["totalNetWorth"]+res["currentStocksInHandValue"]
+                                  -res["totalInvestment"])/res["totalInvestment"]
+            except:
+                res["profitPercent"] = 0
+
+            fin_ans.append(res)
+            
+        df = pd.DataFrame(fin_ans)
+        Q.put(df)
+        Q.put("done")
+    
+    def runMultiprocessing(self,scriptName,saveFile = False):
+        Q = Queue()
+        L = Lock()
+        task = []
+        instances = self.partition 
+        module = importlib.import_module(self.importSource+scriptName)
+        scriptFunction = module.run
+        print("function imported ")
+        partitionList = self.getPartition()
+        stockList = self.allStockList
+
+        for i in range(instances):
+            tempList = stockList[partitionList[i][0]:partitionList[i][1]]
+            task.append(Process(target = self.backtestMultiprocessing,args=(scriptFunction,tempList,Q)))
+            task[i].start()
+        print("Instances started")
+        for i in range(instances):
+            task[i].join()
+
+        counter = 0
+        df = None
+        while Q and counter < instances:
+            val = Q.get()
+            if type(val)==type('') and val == 'done':
+                print("done received")
+                counter+=1 
+            else:
+                if df is None:
+                    df = val
+                else:
+                    df = df.append(val)
+        if saveFile:
+            df.to_csv(os.path.join(self.savePath,scriptName+".csv"),index=False)
+        else:
+            return df
+    
